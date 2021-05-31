61f617d3e824fc5cc7810f1a662179ff917b2414
Rojas <67125669+xrojas06@users.noreply.github.com>
2021-05-31 00:03:10 -0500

    Delete Uno.R

--- Proyecto/Uno.R
+++
@@ -1,8 +0,0 @@
-require(dplyr)
-library(dplyr)
-library(plotly)
-
-datos <- read.csv(file = 'database.csv', header = TRUE, sep = ";")
-View(datos)
-
-db <- aggregate(database[,3:10], list(datos$'Regional indicator'),mean)
